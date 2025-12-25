47f67f162fc31fb8214206b977d71b841223b35b
Kumar Chitransh <chitransh.sudhir@gmail.com>
2025-12-25 16:52:01 +0530

    Update averagesellingprice.sql

--- averagesellingprice.sql
+++ averagesellingprice.sql
@@ -1,16 +1,16 @@
 with cal as (select
 p.product_id,
 us.units as un,
-COALESCE(price*units,0) as pu
+price*units as pu
 from
 prices p
-inner join unitssold us
+left join unitssold us
 on p.product_id = us.product_id
 and us.purchase_date between p.start_date and p.end_date),
 fin as (
     select
     product_id,
-    ROUND(sum(pu)/sum(un),2) as average_price
+    COALESCE(ROUND(sum(pu)/sum(un),2),0) as average_price
     from
     cal
     group by product_id
