10ae5ff1d75201d9230b4238fcd6a7f60e5e9a7c
Kumar Chitransh <chitransh.sudhir@gmail.com>
2026-02-18 00:21:31 +0530

    Refactor SQL query for user activity tracking

--- user-activity-for-past-30-days.sql
+++ user-activity-for-past-30-days.sql
@@ -1,11 +1,10 @@
-# Write your MySQL query statement below
-
-
-select
-activity_date  as day,
-count(distinct user_id) as active_users
-from 
-activity
-where 
-activity_date >= DATE '2019-07-27' - INTERVAL '30' DAY AND activity_date <= DATE '2019-07-27'
-group by activity_date
+SELECT 
+    activity_date AS day, 
+    COUNT(DISTINCT user_id) AS active_users
+FROM 
+    Activity
+WHERE 
+    activity_date > '2019-07-27' - INTERVAL 30 DAY 
+    AND activity_date <= '2019-07-27'
+GROUP BY 
+    activity_date;
